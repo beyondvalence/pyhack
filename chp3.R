735682275d3333c9b41116465517ec7fc70c94b0
wayne <wayne.liu@nih.gov>
2015-10-02 16:03:28 -0400

    added relative file paths and email text parser function

--- chp3.R
+++ chp3.R
@@ -4,3 +4,40 @@
 
 getwd()
 loc <- paste0(getwd(), "/03-Classification/data")
+loc
+
+# 3.1 Binary classification ####
+# this or that, spam or ham
+
+# 3.2 Moving gently into conditional probability ####
+# looking to classify emails as spam or ham
+# Using the Naive Bayes model for conditional probability
+# naive due to assumption of statistical independence
+# assumes word count in emails can be estimated in isolation
+library(tm)
+library(ggplot2)
+
+# set path variables to easy ham, hard ham, spam folders
+spam.path <- '/data/spam'
+spam2.path <- '/data/spam_2'
+easyham.path <- '/data/easy_ham'
+easyham2.path <- '/data/easy_ham_2'
+hardham.path <- '/data/hard_ham'
+hardham2.path <- '/data/hard_ham_2'
+
+# write function that opens email file
+# finds first line break
+# and returns text as character vector
+
+get.msg <- function(path) {
+  con <- file(path, open="rt", encoding="latin1")
+  text <- readLines(con)
+  # The message always begins after the first full line break
+  # finds the line number where the line break occurs
+  # and starts the capture on the next line to the end
+  # to return the text message
+  msg <- text[seq(which(text=="")[1]+1, length(text),1)]
+  close(con)
+  return(paste(msg, collapse="\n"))
+}
+
